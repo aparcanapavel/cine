db93dbb979b38bab0f5ee08a4aa8038b100bca61
aparcanapavel <aparcanapavel@gmail.com>
2020-07-09 17:46:34 -0400

    trying to get window obj

--- src/App.js
+++ src/App.js
@@ -6,9 +6,26 @@ import './App.css';
  */
 
 function App() {
+  // const video = document.querySelector("video");
+
+  const handlePlay = (ele) => {
+    // ele.play();
+    console.log("ele",ele);
+  }
+  
   return (
     <div className="App">
-      Test Test
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          console.log("cool")
+          const currentWindow = window.getCurrent();
+          console.log("currentWindow",currentWindow);
+          const video = document.querySelector("video");
+          debugger;
+          handlePlay(video);
+        }}
+      >Click to play</button>
     </div>
   );
 }
